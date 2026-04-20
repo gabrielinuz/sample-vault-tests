896b36206d9e24342035f81f89c124d201ec1b42
gabrielinuz <gabrielinuz@gmail.com>
2026-04-20 13:47:33 -0300

    Completar pruebas de autenticación de usuario

--- frontend/js/tests/authTests.js
+++ frontend/js/tests/authTests.js
@@ -51,3 +51,20 @@ testUtils.createTestButton("Test Login - Usuario Incorrecto (Juan y 12345)", asy
         testUtils.setSuccess(btn);
     }
 });
+
+testUtils.createTestButton("Test Login - Error en la ruta del Método HTTP", async (btn) => {
+    const response = await fetch('/api/auth/authentication', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: 'pepe', password: '123' }) // Usamos pepe hardcodeado
+    });
+    
+    const data = await response.json();
+    testUtils.log(data);
+
+    if (response.ok) {
+        // Guardamos el token para los siguientes tests de samples
+        localStorage.setItem('test_token', data.token);
+        testUtils.setSuccess(btn);
+    }
+});
